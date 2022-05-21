5d9be2b5377485b2dcc32651515c05155b89b2b9
room-208 <yumalegomindstorm@gmail.com>
2022-05-21 19:48:38 +0900

    defaultを上書きしました。

--- default.cpp
+++ default.cpp
@@ -1,7 +1,4 @@
-//#define _GLIBCXX_DEBUG
 #define _USE_MATH_DEFINES
-#include <bits/stdc++.h>
-#include <time.h>
 
 #include <algorithm>
 #include <atcoder/fenwicktree>
@@ -9,13 +6,15 @@
 #include <atcoder/modint>
 #include <atcoder/scc>
 #include <atcoder/segtree>
+#include <bitset>
 #include <cmath>
 #include <deque>
-#include <fstream>
 #include <functional>
 #include <iomanip>
 #include <iostream>
+#include <limits>
 #include <list>
+#include <map>
 #include <queue>
 #include <random>
 #include <set>
@@ -27,13 +26,13 @@
 using namespace std;
 using namespace atcoder;
 using mint = modint998244353;  // modint1000000007 static_modint<1000000009>;
+using ll = long long;
 
-const int MOD = 1000000007;
-const int INF_int = 1000000000;
-const long long INF_ll = 1000000000000000000LL;
-const int COM_MAX = 510000;
+constexpr int INF_int = 1e9;
+constexpr ll INF_ll = 1e18;
 
-long long fac[COM_MAX], finv[COM_MAX], inv[COM_MAX];
+constexpr int COM_MAX = 1000000;
+mint fac[COM_MAX], finv[COM_MAX], inv[COM_MAX];
 
 // テーブルを作る前処理
 void COMinit() {
@@ -41,37 +40,32 @@ void COMinit() {
   finv[0] = finv[1] = 1;
   inv[1] = 1;
   for (int i = 2; i < COM_MAX; i++) {
-    fac[i] = fac[i - 1] * i % MOD;
-    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
-    finv[i] = finv[i - 1] * inv[i] % MOD;
+    fac[i] = fac[i - 1] * i;
+    inv[i] = -inv[mint::mod() % i] * (mint::mod() / i);
+    finv[i] = finv[i - 1] * inv[i];
   }
 }
 
 // 二項係数計算
-long long COM(int n, int k) {
+mint COM(int n, int k) {
   if (n < k) return 0;
   if (n < 0 || k < 0) return 0;
-  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+  return fac[n] * (finv[k] * finv[n - k]);
 }
 
-//繰り返し二乗法
-long long MOD_pow(long long a, long long n) {
-  long long res = 1;
-  while (n > 0) {
-    if (n & 1) res = res * a % MOD;
-    a = a * a % MOD;
-    n >>= 1;
-  }
-  return res;
+// 順列組合せ
+mint PER(int n, int k) {
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return fac[n] * finv[n - k];
 }
 
 //等差数列の和
-long long tousa_sum(long long a, long long d, long long n) {
-  return (a * 2 + d * (n - 1)) * n / 2;
-}
+// a：初項 d：等差 n：数列の数
+ll tousa_sum(ll a, ll d, ll n) { return (a * 2 + d * (n - 1)) * n / 2LL; }
 
 // 床関数
-long long my_floor(long long a, long long b) {
+ll my_floor(ll a, ll b) {
   assert(b != 0);
   if (b < 0) {
     a *= -1;
@@ -90,7 +84,7 @@ long long my_floor(long long a, long long b) {
 }
 
 // 天井関数
-long long my_ceil(long long a, long long b) {
+ll my_ceil(ll a, ll b) {
   assert(b != 0);
   if (b < 0) {
     a *= -1;
@@ -109,11 +103,11 @@ long long my_ceil(long long a, long long b) {
 }
 
 // 平方関数
-long long my_sqrt(long long x) {
-  long long left = 0;
-  long long right = numeric_limits<int>::max();
+ll my_sqrt(ll x) {
+  ll left = 0;
+  ll right = numeric_limits<int>::max();
   while (right - left > 1) {
-    long long mid = (right + left) / 2;
+    ll mid = (right + left) / 2;
     if (mid * mid <= x) {
       left = mid;
     } else {
@@ -124,10 +118,10 @@ long long my_sqrt(long long x) {
 }
 
 //転倒数
-long long inv_count(const vector<int> &a) {
+ll inv_count(const vector<int> &a) {
   int n = (int)a.size();
   fenwick_tree<int> fw(n);
-  long long ans = 0;
+  ll ans = 0;
   for (int i = 0; i < n; i++) {
     ans += fw.sum(a[i] + 1, n);
     fw.add(a[i], 1);
@@ -136,33 +130,33 @@ long long inv_count(const vector<int> &a) {
 }
 
 //最大公約数
-template <class T>
-T GCD(T m, T n) {
-  // ベースケース
-  if (n == 0) {
-    return m;
+ll GCD(ll a, ll b) {
+  if (b == 0) {
+    return a;
   }
-
-  // 再帰呼び出し
-  return GCD(n, m % n);
+  return GCD(b, a % b);
 }
 
 //拡張GCD
-long long extgcd(long long a, long long b, long long &x, long long &y) {
+ll EXTGCD(ll a, ll b, ll &x, ll &y) {
   if (b == 0) {
     x = 1;
     y = 0;
     return a;
   }
-  long long gcd = extgcd(b, a % b, x, y);
-  long long oldX = x;
-  x = y;
-  y = oldX - a / b * y;
+  ll gcd = EXTGCD(b, a % b, y, x);
+  y -= (a / b) * x;
+  // gcd>0を保証
+  if (gcd < 0) {
+    gcd *= -1;
+    x *= -1;
+    y *= -1;
+  }
   return gcd;
 }
 
 // osa_k法
-vector<int> osa_k(int n) {
+vector<int> OsaK(int n) {
   vector<int> res(n + 1);
   iota(res.begin(), res.end(), 0);
   for (int i = 2; i * i <= n; ++i) {
@@ -179,6 +173,18 @@ vector<int> osa_k(int n) {
     }
   }
   return res;
+}
+
+// 素数を取得する
+vector<int> GetPrime(int n) {
+  vector<int> osak = OsaK(n);
+  vector<int> prime;
+  for (int k = 2; k <= n; k++) {
+    if (k == osak[k]) {
+      prime.push_back(k);
+    }
+  }
+  return prime;
 }
 
 //最大・最小化
@@ -223,18 +229,18 @@ vector<pair<char, int>> runLengthEncoding(string s) {
 //アルファベット表
 vector<vector<int>> alphabet_table(string S) {
   int N = (int)S.size();
-  vector<vector<int>> c(26, vector<int>(N + 1, INF_int));
+  vector<vector<int>> table(26, vector<int>(N + 1, INF_int));
   for (int j = N - 1; j >= 0; j--) {
     int m = S[j] - 'a';
     for (int i = 0; i < 26; i++) {
       if (i == m) {
-        c[i][j] = j;
+        table[i][j] = j;
       } else {
-        c[i][j] = c[i][j + 1];
+        table[i][j] = table[i][j + 1];
       }
     }
   }
-  return c;
+  return table;
 }
 
 // unoderedのハッシュ
@@ -256,65 +262,72 @@ struct HashPair {
   }
 };
 
-// 行列半時計回り90度回転
-template <class T>
-vector<vector<T>> matrix_counter_clockwise(vector<vector<T>> &A, int H, int W) {
-  vector<vector<T>> B(W, vector<T>(H));
-  for (int i = 0; i < H; i++) {
-    for (int j = 0; j < W; j++) {
-      int a = -j + (W - 1);
-      int b = i;
-
-      B[a][b] = A[i][j];
-    }
-  }
-  return B;
-}
-
 //座標圧縮
-void compress(vector<long long> &x) {
-  map<long long, long long> mp;
+vector<ll> compress(vector<ll> &x) {
+  map<ll, ll> mp;
 
   for (int i = 0; i < (int)x.size(); i++) {
     mp[x[i]] = 0;
   }
 
-  long long compress = 1;
+  ll compress = 1;
   for (auto itr = mp.begin(); itr != mp.end(); itr++) {
     itr->second = compress;
     compress++;
   }
 
+  vector<ll> y(x.size());
   for (int i = 0; i < (int)x.size(); i++) {
-    x[i] = mp[x[i]];
+    y[i] = mp[x[i]];
   }
+  return y;
+}
+
+// 最長増加部分列
+vector<int> LIS(const vector<int> &A) {
+  int N = A.size();
+  vector<int> x;
+  vector<int> P(N);
+  for (int i = 0; i < N; i++) {
+    int key = A[i];
+    auto itr = lower_bound(x.begin(), x.end(), key);
+    if (itr == x.end()) {
+      x.push_back(key);
+    } else {
+      *itr = key;
+    }
+    P[i] = x.size();
+  }
+  return P;
 }
 
 //セグ木・遅延セグ木
-// segtree<long long, seg::op, seg::e> sgt;
-// lazy_segtree<long long, seg::op, seg::e, long long, seg::mapping,
+// segtree<ll, seg::op, seg::e> sgt;
+// lazy_segtree<ll, seg::op, seg::e, ll, seg::mapping,
 // seg::composition, seg::id> sgt;
 namespace seg {
-const long long ID = 0;
-long long op(long long a, long long b) { return min(a, b); }
-long long e() { return INF_ll; }
-long long mapping(long long f, long long x) {
+const ll ID = -1;
+ll op(ll a, ll b) { return max(a, b); }
+ll e() { return -INF_ll; }
+ll mapping(ll f, ll x) {
   if (f == ID) {
     return x;
   } else {
-    return x + f;
+    return f;
   }
+  // return f + x;
 }
-long long composition(long long f, long long g) {
+ll composition(ll f, ll g) {
   if (f == ID) {
     return g;
   } else {
-    return f + g;
+    return f;
   }
+  // return f + g;
 }
-long long id() { return ID; }
-long long target;
-bool f(long long v) { return v < target; }
+ll id() { return ID; }
+ll target;
+bool f(ll v) { return v < target; }
 }  // namespace seg
 
 // Union-Find
@@ -360,155 +373,19 @@ struct UnionFind {
 //エッジ集合
 struct Edge {
   int to;
-  long long w;
-  Edge(int to, long long w) : to(to), w(w) {}
+  ll w;
+  Edge(int to, ll w) : to(to), w(w) {}
 };
 
 using Graph_int = vector<vector<int>>;
 using Graph_Edge = vector<vector<Edge>>;
 
-// 深さ優先探索
-void DFS(const Graph_int &G, int v, vector<bool> &seen) {
-  seen[v] = true;
-
-  for (auto next_v : G[v]) {
-    if (seen[next_v]) {
-      continue;
-    }
-
-    DFS(G, next_v, seen);
-  }
-}
-
-//根付き木
-void par_cal(const Graph_int &G, int v, vector<int> &par, int p = -1) {
-  for (auto next_v : G[v]) {
-    if (next_v == p) {
-      continue;
-    }
-
-    par_cal(G, next_v, par, v);
-  }
-
-  par[v] = p;
-}
-
-//部分木サイズ
-void subtree_size_cal(const Graph_int &G, int v, vector<int> &subtree_size,
-                      int p = -1) {
-  for (auto c : G[v]) {
-    if (c == p) {
-      continue;
-    }
-
-    subtree_size_cal(G, c, subtree_size, v);
-  }
-
-  // 帰りがけ時に、部分木サイズを求める
-  subtree_size[v] = 1;  // 自分自身
-  for (auto c : G[v]) {
-    if (c == p) {
-      continue;
-    }
-
-    // 子頂点を根とする部分きのサイズを加算する
-    subtree_size[v] += subtree_size[c];
-  }
-}
-
-//トポロジカルソートのDFS
-void topological_sort_dfs(const Graph_int &G, int v, vector<bool> &seen,
-                          vector<int> &order) {
-  seen[v] = true;
-  for (auto next_v : G[v]) {
-    if (seen[next_v]) {
-      continue;
-    }
-
-    topological_sort_dfs(G, next_v, seen, order);
-  }
-
-  order.push_back(v);
-}
-
-//トポロジカルソート
-void topological_sort(const Graph_int &G, vector<int> &order) {
-  int N = (int)G.size();
-  vector<bool> seen(N, false);
-  for (int v = 0; v < N; v++) {
-    if (seen[v]) {
-      continue;
-    }
-
-    topological_sort_dfs(G, v, seen, order);
-  }
-
-  reverse(order.begin(), order.end());
-}
-
-//幅優先探索
-void BFS(const Graph_int &G, int s) {
-  int N = (int)G.size();    // 頂点数
-  vector<int> dist(N, -1);  // 全頂点を「未訪問」に初期化
-  queue<int> que;
-
-  // 初期条件 (頂点 s を初期頂点とする)
-  dist[s] = 0;
-  que.push(s);  // s を橙色頂点にする
-
-  // BFS 開始 (キューが空になるまで探索を行う)
-  while (!que.empty()) {
-    int v = que.front();  // キューから先頭頂点を取り出す
-    que.pop();
-
-    // v からたどれる頂点をすべて調べる
-    for (int x : G[v]) {
-      // すでに発見済みの頂点は探索しない
-      if (dist[x] != -1) continue;
-
-      // 新たな白色頂点 x について距離情報を更新してキューに挿入
-      dist[x] = dist[v] + 1;
-      que.push(x);
-    }
-  }
-}
-
-// 01BFS
-void BFS_01(const Graph_Edge &G, int s) {
-  int N = (int)G.size();              // 頂点数
-  vector<long long> dist(N, INF_ll);  // 全頂点を「未訪問」に初期化
-  deque<int> que;
-
-  // 初期条件 (頂点 s を初期頂点とする)
-  dist[s] = 0;
-  que.push_front(s);
-
-  // BFS 開始 (キューが空になるまで探索を行う)
-  while (!que.empty()) {
-    int v = que.front();  // キューから先頭頂点を取り出す
-    que.pop_front();
-
-    // v からたどれる頂点をすべて調べる
-    for (auto x : G[v]) {
-      if (chmin(dist[x.to], dist[v] + x.w)) {
-        if (x.w == 0) {
-          que.push_front(x.to);
-        }
-        if (x.w == 1) {
-          que.push_back(x.to);
-        }
-      }
-    }
-  }
-}
-
 class Rerooting {
  private:
   struct DP {
-    long long dp;
-    long long subtree;
+    ll dp;
     DP() {}
-    DP(long long dp, long long subtree) : dp(dp), subtree(subtree) {}
+    DP(ll dp) : dp(dp) {}
   };
 
  public:
@@ -525,24 +402,21 @@ class Rerooting {
     G.resize(N);
   }
   void build() {
-    dfs_init(0);
-    dfs_reroot(0, e());
+    int root = 0;
+    dfs_init(root);
+    dfs_reroot(root, e());
   }
-  void add_edge(int a, int b, long long c) { G[a].push_back(Edge(b, c)); }
+  void add_edge(int a, int b, ll c) { G[a].push_back(Edge(b, c)); }
 
  private:
   // mergeの単位元
-  DP e() { return DP({0, 0}); }
+  DP e() { return DP(0); }
   // マージ
-  DP merge(DP dp_cum, DP dp) {
-    return DP({dp_cum.dp + dp.dp, dp_cum.subtree + dp.subtree});
-  }
+  DP merge(DP dp_cum, DP dp) { return DP(max(dp_cum.dp, dp.dp)); }
   // 頂点に関する計算
-  DP add_root(int v, long long w, DP dp) {
-    return DP({dp.dp + dp.subtree + 1, dp.subtree + 1});
-  }
+  DP add_root(int v, ll w, DP dp) { return DP(dp.dp + w); }
   // 初期解
-  DP dfs_init(int v, long long w = 0, int p = -1) {
+  DP dfs_init(int v, ll w = 0, int p = -1) {
     DP dp_cum = e();
     int degree = G[v].size();
     dp[v].resize(degree);
@@ -582,17 +456,158 @@ class Rerooting {
       if (next_v.to == p) {
         continue;
       }
-      dfs_reroot(next_v.to, add_root(v, next_v.w, merge(S_l[i], S_r[i + 1])),
-                 v);
+      DP dp_v = add_root(v, next_v.w, merge(S_l[i], S_r[i + 1]));
+      dfs_reroot(next_v.to, dp_v, v);
     }
   }
 };
 
+// 深さ優先探索
+void DFS(const Graph_int &G, int v, vector<bool> &seen) {
+  seen[v] = true;
+
+  for (auto next_v : G[v]) {
+    if (seen[next_v]) {
+      continue;
+    }
+
+    DFS(G, next_v, seen);
+  }
+}
+
+//根付き木
+void cal_par(const Graph_int &G, int v, vector<int> &par, int p = -1) {
+  for (auto next_v : G[v]) {
+    if (next_v == p) {
+      continue;
+    }
+
+    cal_par(G, next_v, par, v);
+  }
+
+  par[v] = p;
+}
+
+//部分木サイズ
+void cal_subtree_size(const Graph_int &G, int v, vector<int> &subtree_size,
+                      int p = -1) {
+  for (auto c : G[v]) {
+    if (c == p) {
+      continue;
+    }
+
+    cal_subtree_size(G, c, subtree_size, v);
+  }
+
+  // 帰りがけ時に、部分木サイズを求める
+  subtree_size[v] = 1;  // 自分自身
+  for (auto c : G[v]) {
+    if (c == p) {
+      continue;
+    }
+
+    // 子頂点を根とする部分きのサイズを加算する
+    subtree_size[v] += subtree_size[c];
+  }
+}
+
+//トポロジカルソート
+class TopologicalSort {
+ private:
+  void dfs(const Graph_int &G, int v, vector<bool> &seen, vector<int> &order) {
+    seen[v] = true;
+    for (auto next_v : G[v]) {
+      if (seen[next_v]) {
+        continue;
+      }
+
+      dfs(G, next_v, seen, order);
+    }
+
+    order.push_back(v);
+  }
+
+ public:
+  vector<int> sort(const Graph_int &G) {
+    int N = (int)G.size();
+    vector<int> order;
+    vector<bool> seen(N, false);
+    for (int v = 0; v < N; v++) {
+      if (seen[v]) {
+        continue;
+      }
+
+      dfs(G, v, seen, order);
+    }
+
+    reverse(order.begin(), order.end());
+    return order;
+  }
+};
+
+//幅優先探索
+vector<int> BFS(const Graph_int &G, int s) {
+  int N = (int)G.size();    // 頂点数
+  vector<int> dist(N, -1);  // 全頂点を「未訪問」に初期化
+  queue<int> que;
+
+  // 初期条件 (頂点 s を初期頂点とする)
+  dist[s] = 0;
+  que.push(s);  // s を橙色頂点にする
+
+  // BFS 開始 (キューが空になるまで探索を行う)
+  while (!que.empty()) {
+    int v = que.front();  // キューから先頭頂点を取り出す
+    que.pop();
+
+    // v からたどれる頂点をすべて調べる
+    for (int x : G[v]) {
+      // すでに発見済みの頂点は探索しない
+      if (dist[x] != -1) continue;
+
+      // 新たな白色頂点 x について距離情報を更新してキューに挿入
+      dist[x] = dist[v] + 1;
+      que.push(x);
+    }
+  }
+  return dist;
+}
+
+// 01BFS
+vector<ll> BFS_01(const Graph_Edge &G, int s) {
+  int N = (int)G.size();       // 頂点数
+  vector<ll> dist(N, INF_ll);  // 全頂点を「未訪問」に初期化
+  deque<int> que;
+
+  // 初期条件 (頂点 s を初期頂点とする)
+  dist[s] = 0;
+  que.push_front(s);
+
+  // BFS 開始 (キューが空になるまで探索を行う)
+  while (!que.empty()) {
+    int v = que.front();  // キューから先頭頂点を取り出す
+    que.pop_front();
+
+    // v からたどれる頂点をすべて調べる
+    for (auto x : G[v]) {
+      if (chmin(dist[x.to], dist[v] + x.w)) {
+        if (x.w == 0) {
+          que.push_front(x.to);
+        }
+        if (x.w == 1) {
+          que.push_back(x.to);
+        }
+      }
+    }
+  }
+  return dist;
+}
+
 //ベルマン・フォード法
-void Bellman_Ford(const Graph_Edge &G, int s) {
+vector<ll> Bellman_Ford(const Graph_Edge &G, int s) {
   int N = (int)G.size();
   bool exist_negative_cycle = false;  // 負閉路をもつかどうか
-  vector<long long> dist(N, INF_ll);
+  vector<ll> dist(N, INF_ll);
   dist[s] = 0;
 
   for (int iter = 0; iter < N; ++iter) {
@@ -615,17 +630,17 @@ void Bellman_Ford(const Graph_Edge &G, int s) {
     // N 回目の反復で更新が行われたならば，負閉路をもつ
     if (iter == N - 1 && update) exist_negative_cycle = true;
   }
+  return dist;
 }
 
 //ダイクストラ法
-void Dijkstra(const Graph_Edge &G, int s) {
+vector<ll> Dijkstra(const Graph_Edge &G, int s) {
   int N = (int)G.size();
-  vector<long long> dist(N, INF_ll);
+  vector<ll> dist(N, INF_ll);
   dist[s] = 0;
 
   // (d[v], v) のペアを要素としたヒープを作る
-  priority_queue<pair<long long, int>, vector<pair<long long, int>>,
-                 greater<pair<long long, int>>>
+  priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>>
       que;
   que.push(make_pair(dist[s], s));
 
@@ -634,7 +649,7 @@ void Dijkstra(const Graph_Edge &G, int s) {
     // v: 使用済みでない頂点のうち d[v] が最小の頂点
     // d: v に対するキー値
     int v = que.top().second;
-    long long d = que.top().first;
+    ll d = que.top().first;
     que.pop();
 
     // d > dist[v] は，(d, v) がゴミであることを意味する
@@ -648,11 +663,49 @@ void Dijkstra(const Graph_Edge &G, int s) {
       }
     }
   }
+  return dist;
 }
 
-//オーバーフロー判定
+// ワーシャル・フロイド法
+vector<vector<ll>> WarshallFloyd(vector<vector<ll>> &w) {
+  vector<vector<ll>> dist = w;
+  int N = dist.size();
+  for (int k = 0; k < N; k++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+      }
+    }
+  }
+  return dist;
+}
+
+// ソート済ペア
+template <class Ta, class Tb>
+pair<vector<Ta>, vector<Tb>> GetSortedPair(vector<Ta> a, vector<Tb> b,
+                                           bool reverse_flag = false) {
+  assert(a.size() == b.size());
+  int N = a.size();
+  vector<pair<Ta, Tb>> p(N);
+  for (int i = 0; i < N; i++) {
+    p[i] = {a[i], b[i]};
+  }
+  sort(p.begin(), p.end());
+  if (reverse_flag) {
+    reverse(p.begin(), p.end());
+  }
+  for (int i = 0; i < N; i++) {
+    a[i] = p[i].first;
+    b[i] = p[i].second;
+  }
+  return {a, b};
+}
+
+//組み込み関数（GCC）
 //__builtin_add_overflow
 //__builtin_mul_overflow
+//__builtin_popcount()
+//__builtin_popcountll()
 
 //ノード変換
 int to_node(int i, int j, int W) { return W * i + j; }
@@ -666,13 +719,16 @@ pair<int, int> to_ij(int v, int W) {
 }
 
 // in_out判定
-bool in_out(int i, int j, int H, int W) {
+bool Is_in(int i, int j, int H, int W) {
   if (0 <= i && i < H && 0 <= j && j < W) {
     return true;
   }
 
   return false;
 }
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
 struct my_struct {
   int a, b;
